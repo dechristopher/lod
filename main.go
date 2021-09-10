9b5250e12d53ad09f140e40547098a0094b499cd
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-09-10 09:38:49 -0400

    feat: main.go implementation

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	"github.com/tile-fund/lod/env"
+	"github.com/tile-fund/lod/str"
+	"github.com/tile-fund/lod/util"
+	"github.com/tile-fund/lod/www"
+)
+
+// init parses flags, sets constants, and prepares us for battle
+func init() {
+	fmt.Printf("LOD v%s", util.Version)
+
+	// set boot time immediately
+	util.BootTime = time.Now()
+
+	// parse command line flags
+	util.DebugFlagPtr = flag.String(str.FDebugFlags, "", str.FDebugFlagsUsage)
+	flag.Parse()
+
+	// parse out debug flags from command line options
+	util.DebugFlags = strings.Split(*util.DebugFlagPtr, ",")
+
+	if env.IsDev() {
+		util.Info(str.CMain, str.MDevMode)
+	}
+}
+
+// main does the things
+func main() {
+	// load .env if any
+	_ = godotenv.Load()
+
+	www.Serve()
+}
