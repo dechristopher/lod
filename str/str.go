0129eac42d6ba954021abf7ad452d93f49cb814a
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-09-10 09:35:26 -0400

    feat: internal strings package

---
+++ str/str.go
@@ -0,0 +1,46 @@
+package str
+
+// CPadding is the current max caller padding, dynamically increased
+var CPadding = 0
+
+// Flags, log formats and miscellaneous strings
+const (
+	FDebugFlags      = "debug"
+	FDebugFlagsUsage = "Comma separated debug flags [foo,bar,baz]"
+
+	InfoFormat  = "INFO  [%s] %s\n"
+	DebugFormat = "DEBUG [%s] %s\n"
+	ErrorFormat = "ERROR [%s] %s\n"
+)
+
+// (C) Log caller names
+const (
+	CMain = "LIO"
+	CLog  = "Log"
+	CTool = "Tool"
+)
+
+// (E) Error messages
+const (
+	ELogFail = "failed to log error=%s msg=%+v"
+	ERead    = "read err: %s"
+	EWrite   = "write err: meta=%+v error=%s"
+)
+
+// (U) User-facing error messages and codes
+const ()
+
+// (M) Standard info log messages
+const (
+	MDevMode  = "!! DEVELOPER MODE !!"
+	MInit     = "starting %s"
+	MStarted  = "started in %s [env: %s][http: %s][health: %s]"
+	MShutdown = "shutting down"
+	MExit     = "exit"
+)
+
+// (D) Debug log messages
+const ()
+
+// (T) Test messages
+const ()
