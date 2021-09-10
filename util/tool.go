d6fad052767350e8dde12d52038e9c123fcc7d94
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-09-10 09:35:11 -0400

    feat: basic tooling utilities

---
+++ util/tool.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// GenerateCode generates an n character seeded randomized code
+func GenerateCode(length int) string {
+	b := make([]byte, length)
+	for {
+		for i := range b {
+			b[i] = charset[seededRand.Intn(len(charset))]
+		}
+	}
+}
+
+// MilliTime returns the current millisecond time
+func MilliTime() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+// TimeSinceBoot returns the time elapsed since process boot
+func TimeSinceBoot() time.Duration {
+	return time.Since(BootTime)
+}
