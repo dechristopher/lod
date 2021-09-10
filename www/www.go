192eb568a32794ade7d72d317e7be517fdd48b02
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-09-10 09:38:19 -0400

    feat: fiber based http server with graceful shutdown

---
+++ www/www.go
@@ -0,0 +1,72 @@
+package www
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+
+	"github.com/tile-fund/lod/env"
+	"github.com/tile-fund/lod/str"
+	"github.com/tile-fund/lod/util"
+	"github.com/tile-fund/lod/www/handlers"
+	"github.com/tile-fund/lod/www/middleware"
+)
+
+// Serve all public endpoints
+func Serve() {
+	util.Info(str.CMain, str.MInit, util.Version)
+
+	r := fiber.New(fiber.Config{
+		ServerHeader:          "LODv" + util.Version,
+		CaseSensitive:         true,
+		ErrorHandler:          nil,
+		DisableStartupMessage: true,
+	})
+
+	// wire up all route handlers
+	wireHandlers(r)
+
+	// Graceful shutdown with SIGINT
+	// SIGTERM and others will hard kill
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		_ = <-c
+		util.Info(str.CMain, str.MShutdown)
+		_ = r.Shutdown()
+	}()
+
+	util.Info(str.CMain, str.MStarted, util.TimeSinceBoot(),
+		env.GetEnv(), util.GetPort(), "none")
+
+	// listen for connections on primary listening port
+	if err := r.Listen(util.GetListenPort()); err != nil {
+		log.Println(err)
+	}
+
+	// Exit cleanly
+	util.Info(str.CMain, str.MExit)
+	os.Exit(0)
+}
+
+// wireHandlers builds all the websocket and http routes
+// into the fiber app context
+func wireHandlers(r *fiber.App) {
+	// recover from panics
+	r.Use(recover.New())
+
+	// wire up all middleware components
+	middleware.Wire(r)
+
+	// root handler to show configured endpoints
+	// r.Get("/", TODO)
+
+	// JSON service health / status handler
+	r.Get("/lio", handlers.StatusHandler)
+
+	// Custom 404 page
+	middleware.NotFound(r)
+}
