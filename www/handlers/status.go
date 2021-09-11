7100ca0042d6f2f52f49cb469252717eb9140a79
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-09-10 22:40:49 -0400

    feat: added environment to status endpoint

--- www/handlers/status.go
+++ www/handlers/status.go
@@ -2,21 +2,24 @@ package handlers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/tile-fund/lod/env"
 
 	"github.com/tile-fund/lod/util"
 )
 
 type status struct {
-	Version  string  `json:"v"`      // current lio version
-	Uptime   float64 `json:"uptime"` // uptime in seconds
-	BootTime int64   `json:"boot"`   // time started, unix timestamp
+	Version     string  `json:"v"`      // current lio version
+	Environment env.Env `json:"env"`    // configured environment
+	Uptime      float64 `json:"uptime"` // uptime in seconds
+	BootTime    int64   `json:"boot"`   // time started, unix timestamp
 }
 
 // StatusHandler returns a JSON object with status info
 func StatusHandler(c *fiber.Ctx) error {
 	return c.JSON(status{
-		Version:  util.Version,
-		Uptime:   util.TimeSinceBoot().Seconds(),
-		BootTime: util.BootTime.UnixMilli(),
+		Version:     util.Version,
+		Environment: env.GetEnv(),
+		Uptime:      util.TimeSinceBoot().Seconds(),
+		BootTime:    util.BootTime.UnixMilli(),
 	})
 }
