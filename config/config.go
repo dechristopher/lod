5c44f6fdf8613528647a1d7e799ec465727e64b9
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-09-15 13:05:01 -0400

    feat: added config validation, memTTL, and headers

--- config/config.go
+++ config/config.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/BurntSushi/toml"
+	"github.com/gofiber/fiber/v2"
+	"github.com/pkg/errors"
 	"github.com/tile-fund/lod/env"
 	"github.com/tile-fund/lod/str"
 	"github.com/tile-fund/lod/util"
@@ -38,20 +42,35 @@ type Instance struct {
 
 // Proxy represents a configuration for a single endpoint proxy instance
 type Proxy struct {
-	Name        string `json:"name" toml:"name"`                 // display name for this proxy
-	TileURL     string `json:"tile_url" toml:"tile_url"`         // templated tileserver URL that this instance will hit
-	CorsOrigins string `json:"cors_origins" toml:"cors_origins"` // allowed CORS origins, comma separated
-	AccessToken string `json:"-" toml:"access_token"`            // optional access token for incoming requests
-	Cache       Cache  `json:"cache" toml:"cache"`               // cache configuration for this proxy instance
+	Name        string   `json:"name" toml:"name"`                 // display name for this proxy
+	TileURL     string   `json:"tile_url" toml:"tile_url"`         // templated tileserver URL that this instance will hit
+	CorsOrigins string   `json:"cors_origins" toml:"cors_origins"` // allowed CORS origins, comma separated
+	Headers     []string `json:"headers" toml:"headers"`           // additional headers to pull through from the tileserver
+	AccessToken string   `json:"-" toml:"access_token"`            // optional access token for incoming requests
+	Cache       Cache    `json:"cache" toml:"cache"`               // cache configuration for this proxy instance
 }
 
 // Cache configuration for a Proxy instance
 type Cache struct {
 	MemCap   int `json:"mem_cap" toml:"mem_cap"`     // maximum number of tiles to store in the in-memory LRU cache
 	MemPrune int `json:"mem_prune" toml:"mem_prune"` // number tiles to prune when we hit the MemCap
+	MemTTL   int `json:"mem_ttl" toml:"mem_ttl"`     // in-memory cache TTL
 	// Note: our redis cache does not have a max cap on tiles. It will grow unbounded, so
 	// you must use a TTL to avoid capping out your cluster if you have a large tile set.
 	RedisTTL int `json:"redis_ttl" toml:"redis_ttl"` // redis tile cache TTL in seconds (or -1 for no TTL)
+}
+
+var defaultCache = Cache{
+	MemCap:   1000,
+	MemPrune: 50,
+	MemTTL:   86400,
+	RedisTTL: 604800,
+}
+
+var zeroCache = Cache{
+	MemCap:   0,
+	MemPrune: 0,
+	RedisTTL: 0,
 }
 
 // Read config file into Capabilities
@@ -61,14 +80,125 @@ func Read() error {
 		return err
 	}
 
-	_, err = toml.Decode(string(configData), &Cap)
+	if _, err = toml.Decode(string(configData), &Cap); err != nil {
+		return err
+	}
 
-	Cap.Version = Version
+	// set default cache parameters if not provided
+	for _, proxy := range Cap.Proxies {
+		if proxy.Cache == zeroCache {
+			proxy.Cache = defaultCache
+		}
+
+		// Register default content headers
+		proxy.registerHeader("Content-Type")
+		proxy.registerHeader("Content-Encoding")
+	}
+
+	// inject instance info to config for viewing in /capabilities
 	Cap.Instance.Environment = string(env.GetEnv())
-	return err
+	Cap.Version = Version
+
+	// validate configuration
+	return validate(Cap)
+}
+
+// validate instance Capabilities for sanity and errors
+func validate(c Capabilities) error {
+	if c.Instance.Port == 0 {
+		return errors.New(fmt.Sprintf("invalid port provided port=%d", c.Instance.Port))
+	}
+
+	// validate each provided proxy endpoint configuration
+	for num, proxy := range c.Proxies {
+		if err := validateProxy(num, proxy); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// registerHeader will add a header to the list of headers to pull through from
+// the underlying configured tileserver
+func (p Proxy) registerHeader(header string) {
+	found := false
+	for _, key := range p.Headers {
+		if key == header {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		p.Headers = append(p.Headers, header)
+	}
+}
+
+// PopulateHeaders will fill the given header map with configured headers
+func (p Proxy) PopulateHeaders(c *fiber.Ctx, headers map[string]string) {
+	for _, header := range p.Headers {
+		if len(c.Response().Header.Peek(header)) > 0 {
+			headers[header] = string(c.Response().Header.Peek(header))
+		}
+	}
+}
+
+// validateProxy will validate an individual proxy endpoint in the configuration
+func validateProxy(num int, proxy Proxy) error {
+	if proxy.Name == "" {
+		return errors.New(fmt.Sprintf("configured proxy #%d cannot have an empty name", num))
+	}
+
+	matched, err := regexp.Match("^[a-zA-Z0-9_]+$", []byte(proxy.Name))
+	if err != nil {
+		return errors.Wrapf(err, "configured proxy name error name=%s", proxy.Name)
+	}
+	if !matched {
+		return errors.New(fmt.Sprintf("configured proxy name may only "+
+			"contain alphanumerics and underscores name=%s", proxy.Name))
+	}
+
+	if !strings.Contains(proxy.TileURL, "{z}") {
+		return errors.New(fmt.Sprintf("proxy tile endpoint URL does not contain "+
+			"{z} placeholder proxy=%s url=%s", proxy.Name, proxy.TileURL))
+	}
+
+	if !strings.Contains(proxy.TileURL, "{x}") {
+		return errors.New(fmt.Sprintf("proxy tile endpoint URL does not contain "+
+			"{x} placeholder proxy=%s url=%s", proxy.Name, proxy.TileURL))
+	}
+
+	if !strings.Contains(proxy.TileURL, "{y}") {
+		return errors.New(fmt.Sprintf("proxy tile endpoint URL does not contain "+
+			"{y} placeholder proxy=%s url=%s", proxy.Name, proxy.TileURL))
+	}
+
+	return validateCache(proxy)
+}
+
+// validateCache will validate a proxy endpoint's cache configuration
+func validateCache(proxy Proxy) error {
+	if proxy.Cache.MemCap < 1 {
+		return errors.New(fmt.Sprintf("proxy cache cannot have zero or negative "+
+			"capacity proxy=%s", proxy.Name))
+	}
+
+	if proxy.Cache.MemPrune < 1 {
+		return errors.New(fmt.Sprintf("proxy cache cannot have zero or negative "+
+			"prune size proxy=%s", proxy.Name))
+	}
+
+	if proxy.Cache.RedisTTL < 1 {
+		return errors.New(fmt.Sprintf("proxy cache cannot have zero or negative "+
+			"redis TTL proxy=%s", proxy.Name))
+	}
+
+	return nil
 }
 
 // GetPort returns the configured primary HTTP port
+// or 1337 if none configured
 func GetPort() int {
 	portEnv, ok := os.LookupEnv("PORT")
 	if !ok {
@@ -90,10 +220,13 @@ func GetListenPort() string {
 }
 
 // CorsOrigins returns the proper CORS origin configuration
-//  or "*" if none configured
-func CorsOrigins() string {
+// or "*" if none configured
+func CorsOrigins(p Proxy) string {
 	origins, ok := os.LookupEnv("CORS")
 	if !ok {
+		if p.CorsOrigins != "" {
+			return p.CorsOrigins
+		}
 		return "*"
 	}
 
