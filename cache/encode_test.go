31348a2b07aaaff7d527467b3b484c686cf8c762
Andrew DeChristopher <andrewdechristopher@gmail.com>
2021-09-18 18:00:21 -0400

    feat: implemented tile packet encoding test for no headers

--- cache/encode_test.go
+++ cache/encode_test.go
@@ -49,3 +49,30 @@ func TestEncode(t *testing.T) {
 		t.Errorf(str.TCacheBadValidation)
 	}
 }
+
+// TestEncode will test that a given tile without metadata encodes properly
+func TestEncodeNoHeaders(t *testing.T) {
+	headers := map[string]string{}
+
+	// encode test tile
+	tile := testCache.Encode("7/37/47", testTile, headers)
+
+	// test the header counter was encoded properly
+	if tile.LenHeaders() != len(headers) {
+		t.Errorf(str.TCacheEncodeHeaders, tile.LenHeaders(), len(headers))
+	}
+
+	if !reflect.DeepEqual(tile.Headers(), headers) {
+		t.Errorf(str.TCacheBadHeaderData)
+	}
+
+	// test that tile data was encoded properly
+	if !reflect.DeepEqual(tile.TileData(), testTile) {
+		t.Errorf(str.TCacheBadTileData)
+	}
+
+	// ensure checksum is computed properly and matches stored checksum
+	if !tile.Validate() {
+		t.Errorf(str.TCacheBadValidation)
+	}
+}
