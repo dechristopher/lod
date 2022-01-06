4d9b3352de1f064061b8289d7d8b464e7b2fc51a
Andrew DeChristopher <andrewdechristopher@gmail.com>
2022-01-05 19:05:48 -0500

    fix: missing comment in ErrParamNameDuplicate.Error

--- config/errors.go
+++ config/errors.go
@@ -7,6 +7,7 @@ type ErrParamNameDuplicate struct {
 	Parameter Param
 }
 
+// Error returns the string representation of ErrParamNameDuplicate
 func (e ErrParamNameDuplicate) Error() string {
 	return fmt.Sprintf("config: proxy '%s' redeclares duplicate parameter with name '%s'",
 		e.ProxyName, e.Parameter.Name)
