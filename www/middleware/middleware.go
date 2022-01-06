f9b8b628d2d583f4a9118de9da1ac591bfefde3a
Andrew DeChristopher <andrewdechristopher@gmail.com>
2022-01-06 00:15:13 -0500

    fix: don't log in all middleware wiring

--- www/middleware/middleware.go
+++ www/middleware/middleware.go
@@ -1,24 +1,12 @@
 package middleware
 
 import (
-	"os"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/tile-fund/lod/config"
-
-	"github.com/tile-fund/lod/env"
 )
-
-const logFormatProd = "${ip} ${header:x-forwarded-for} ${header:x-real-ip} " +
-	"[${time}] ${pid} ${locals:requestid}${locals:lod-cache} \"${method} ${path} ${protocol}\" " +
-	"${status} ${latency} ${bytesSent}b \"${referrer}\" \"${ua}\"\n"
-
-const logFormatDev = "${ip} [${time}]${locals:lod-cache} \"${method} ${path} ${protocol}\" " +
-	"${status} ${latency} ${bytesSent}b\n"
 
 // Wire attaches all middleware to the given router
 func Wire(r fiber.Router, proxy ...config.Proxy) {
@@ -38,14 +26,6 @@ func Wire(r fiber.Router, proxy ...config.Proxy) {
 			AllowHeaders: "Origin, Content-Type, Accept",
 		}))
 	}
-
-	// STDOUT request logger
-	r.Use(logger.New(logger.Config{
-		TimeZone:   "local",
-		TimeFormat: "2006-01-02T15:04:05-0700",
-		Format:     logFormat(),
-		Output:     os.Stdout,
-	}))
 }
 
 // NotFound wires the final 404 handler after all other
@@ -54,13 +34,4 @@ func NotFound(r *fiber.App) {
 	r.Use(func(c *fiber.Ctx) error {
 		return c.SendStatus(404)
 	})
-}
-
-// logFormat returns the HTTP log format for the
-// configured fiber logger middleware
-func logFormat() string {
-	if env.IsProd() {
-		return logFormatProd
-	}
-	return logFormatDev
 }
