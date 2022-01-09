7f085a3b7d2b79f4e501dd98f0da967b9127b87b
Andrew DeChristopher <andrewdechristopher@gmail.com>
2022-01-08 19:02:13 -0500

    fix: cleaned up proxy endpoint handler

--- www/handlers/proxy/proxy.go
+++ www/handlers/proxy/proxy.go
@@ -10,7 +10,6 @@ import (
 	"github.com/tile-fund/lod/helpers"
 	"github.com/tile-fund/lod/str"
 	"github.com/tile-fund/lod/util"
-	"github.com/tile-fund/lod/www/middleware"
 )
 
 type tileError struct {
@@ -18,122 +17,89 @@ type tileError struct {
 	proxy config.Proxy
 }
 
-// wireProxy configures a new proxy endpoint from the configuration under
-// a named Router group
-func wireProxy(r *fiber.App, p config.Proxy) {
-	// handler group for this proxy instance
-	proxyGroup := r.Group(p.Name)
-
-	// wire middleware for proxy group
-	middleware.Wire(r, p)
-
-	// configure CORS preflight handler
-	proxyGroup.Options("/:z/:x/:y.*", preflight)
-
-	// configure proxy endpoint handler
-	proxyGroup.Get("/:z/:x/:y.*", handler(p))
-
-	// set common cors headers after handlers to override response from upstream
-	proxyGroup.Use(corsHeaders(p))
-}
-
-// corsHeaders sets cord headers after proxy handler execution
-func corsHeaders(p config.Proxy) fiber.Handler {
-	return func(ctx *fiber.Ctx) error {
-		// Set CORS allow methods
-		ctx.Set("Access-Control-Allow-Methods", "GET, OPTIONS")
-		// Set CORS origin headers
-		ctx.Set("Access-Control-Allow-Origin", config.CorsOrigins(p))
-		return nil
-	}
-}
-
-// preflight handler for CORS OPTIONS requests
-func preflight(ctx *fiber.Ctx) error {
-	// Tell client that this pre-flight info is valid for 20 days
-	ctx.Set("Access-Control-Max-Age", "1728000")
-	ctx.Set("Content-Type", "text/plain charset=UTF-8")
-	ctx.Set("Content-Length", "0")
-	return ctx.SendStatus(fiber.StatusNoContent)
-}
-
-// Build a new proxy endpoint handler from configuration
-func handler(p config.Proxy) fiber.Handler {
+// genHandler builds a new proxy endpoint handler from configuration
+func genHandler(p config.Proxy) fiber.Handler {
 	// preconfigure cache on boot
 	cache.Get(p.Name)
 
+	// handler function to wire to endpoint
 	return func(ctx *fiber.Ctx) error {
-		// check presence of configured URL parameters and store
-		// their values in a map within the request locals
-		helpers.FillParamsMap(p, ctx)
+		return handle(p, ctx)
+	}
+}
 
-		// calculate url from the configured URL and params
-		tileUrl, err := buildTileUrl(p, ctx)
-		if err != nil {
-			ctx.Locals("lod-cache", " :err ")
-			util.Error(str.CProxy, str.EBadRequest, err.Error())
-			return ctx.SendStatus(fiber.StatusBadRequest)
-		}
+// handle proxy requests for the specified proxy config
+func handle(p config.Proxy, ctx *fiber.Ctx) error {
+	// check presence of configured URL parameters and store
+	// their values in a map within the request locals
+	helpers.FillParamsMap(p, ctx)
 
-		// calculate the cache key for this request using XYZ and URL params
-		cacheKey, err := helpers.BuildCacheKey(p, ctx)
+	// calculate url from the configured URL and params
+	tileUrl, err := buildTileUrl(p, ctx)
+	if err != nil {
+		ctx.Locals("lod-cache", " :err ")
+		util.Error(str.CProxy, str.EBadRequest, err.Error())
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	// calculate the cache key for this request using XYZ and URL params
+	cacheKey, err := helpers.BuildCacheKey(p, ctx)
+	if err != nil {
+		ctx.Locals("lod-cache", "  :err")
+		util.Error(str.CProxy, str.ECacheBuildKey, err.Error())
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	if cachedTile := cache.Get(p.Name).Fetch(cacheKey); cachedTile != nil {
+		// IF WE HIT A CACHED TILE
+		// write the tile to the response body
+		_, err := ctx.Write(cachedTile.TileData())
 		if err != nil {
 			ctx.Locals("lod-cache", "  :err")
-			util.Error(str.CProxy, str.ECacheBuildKey, err.Error())
+			util.Error(str.CProxy, str.EWrite, err.Error(), tileError{
+				url:   tileUrl,
+				proxy: p,
+			})
 			return ctx.SendStatus(fiber.StatusInternalServerError)
 		}
 
-		if cachedTile := cache.Get(p.Name).Fetch(cacheKey); cachedTile != nil {
-			// IF WE HIT A CACHED TILE
-			// write the tile to the response body
-			_, err := ctx.Write(cachedTile.TileData())
-			if err != nil {
-				ctx.Locals("lod-cache", "  :err")
-				util.Error(str.CProxy, str.EWrite, err.Error(), tileError{
-					url:   tileUrl,
-					proxy: p,
-				})
-				return ctx.SendStatus(fiber.StatusInternalServerError)
-			}
+		ctx.Locals("lod-cache", " :hit ")
 
-			ctx.Locals("lod-cache", " :hit ")
-
-			// set stored headers in response
-			for key, val := range cachedTile.Headers() {
-				ctx.Set(key, val)
-			}
-		} else {
-			// IF WE MISSED A CACHED TILE
-			ctx.Locals("lod-cache", " :miss")
-			// perform request to tile URL
-			if err := proxy.Do(ctx, tileUrl); err != nil {
-				return err
-			}
-
-			if len(ctx.Response().Body()) > 0 {
-				// copy tile data into separate slice, so we don't lose the reference
-				tileData := make([]byte, len(ctx.Response().Body()))
-				copy(tileData, ctx.Response().Body())
-
-				headers := map[string]string{}
-				// Store configured headers into the tile cache for this tile
-				p.PopulateHeaders(ctx, headers)
-
-				// Delete headers from the final response that are on the DelHeaders list
-				// if we got them from the tileserver. This can be used to prevent leaking
-				// internals of the tileserver if you don't control what it returns
-				p.DeleteHeaders(ctx)
-
-				// spin off a routine to cache the tile without blocking the response
-				go cache.Get(p.Name).EncodeSet(cacheKey, tileData, headers)
-			}
+		// set stored headers in response
+		for key, val := range cachedTile.Headers() {
+			ctx.Set(key, val)
+		}
+	} else {
+		// IF WE MISSED A CACHED TILE
+		ctx.Locals("lod-cache", " :miss")
+		// perform request to tile URL
+		if err := proxy.Do(ctx, tileUrl); err != nil {
+			return err
 		}
 
-		// Remove server header from response
-		ctx.Response().Header.Del(fiber.HeaderServer)
+		if len(ctx.Response().Body()) > 0 {
+			// copy tile data into separate slice, so we don't lose the reference
+			tileData := make([]byte, len(ctx.Response().Body()))
+			copy(tileData, ctx.Response().Body())
 
-		return ctx.Next()
+			headers := map[string]string{}
+			// Store configured headers into the tile cache for this tile
+			p.PopulateHeaders(ctx, headers)
+
+			// Delete headers from the final response that are on the DelHeaders list
+			// if we got them from the tileserver. This can be used to prevent leaking
+			// internals of the tileserver if you don't control what it returns
+			p.DeleteHeaders(ctx)
+
+			// spin off a routine to cache the tile without blocking the response
+			go cache.Get(p.Name).EncodeSet(cacheKey, tileData, headers)
+		}
 	}
+
+	// Remove server header from response
+	ctx.Response().Header.Del(fiber.HeaderServer)
+
+	return ctx.Next()
 }
 
 // buildTileUrl will substitute URL tile params into the proxy tile URL
--- www/handlers/proxy/wire.go
+++ www/handlers/proxy/wire.go
@@ -5,6 +5,7 @@ import (
 	"github.com/tile-fund/lod/config"
 	"github.com/tile-fund/lod/str"
 	"github.com/tile-fund/lod/util"
+	"github.com/tile-fund/lod/www/middleware"
 )
 
 // Wire proxy group and endpoints for each configured proxy
@@ -13,4 +14,43 @@ func Wire(r *fiber.App) {
 		wireProxy(r, p)
 		util.Info(str.CMain, str.MProxy, p.Name, p.TileURL)
 	}
+}
+
+// wireProxy configures a new proxy endpoint from the configuration under
+// a named Router group
+func wireProxy(r *fiber.App, p config.Proxy) {
+	// genHandler group for this proxy instance
+	proxyGroup := r.Group(p.Name)
+
+	// wire middleware for proxy group
+	middleware.Wire(r, p)
+
+	// configure CORS preflight genHandler
+	proxyGroup.Options("/:z/:x/:y.*", preflight)
+
+	// configure proxy endpoint genHandler
+	proxyGroup.Get("/:z/:x/:y.*", genHandler(p))
+
+	// set common cors headers after handlers to override response from upstream
+	proxyGroup.Use(corsHeaders(p))
+}
+
+// corsHeaders sets cord headers after proxy genHandler execution
+func corsHeaders(p config.Proxy) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		// Set CORS allow methods
+		ctx.Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		// Set CORS origin headers
+		ctx.Set("Access-Control-Allow-Origin", config.CorsOrigins(p))
+		return nil
+	}
+}
+
+// preflight genHandler for CORS OPTIONS requests
+func preflight(ctx *fiber.Ctx) error {
+	// Tell client that this pre-flight info is valid for 20 days
+	ctx.Set("Access-Control-Max-Age", "1728000")
+	ctx.Set("Content-Type", "text/plain charset=UTF-8")
+	ctx.Set("Content-Length", "0")
+	return ctx.SendStatus(fiber.StatusNoContent)
 }
